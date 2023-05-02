a5a85f611428b09d336b98afdf59a654d79165c1
ikuma-t <tadokorodev@gmail.com>
2023-05-02 12:51:44 +0900

    test: remove unused test case

--- src/lib/__test__/dateUtil.test.ts
+++ src/lib/__test__/dateUtil.test.ts
@@ -30,28 +30,6 @@ describe("format Date (With TZ)", () => {
   );
 });
 
-describe("format Date (Without TZ)", () => {
-  test.each<[string, string, string]>([
-    ["2023-01-01", "Asia/Tokyo", "2023-01-01T00:00:00+09:00"],
-  ])(
-    `formatDate(%s, %s, ${DATE_FORMAT.ISO8601}): %s`,
-    (dateString, timezone, expected) => {
-      const dateFormat = DATE_FORMAT.ISO8601;
-      expect(formatUTCDate(dateString, timezone, dateFormat)).toBe(expected);
-    }
-  );
-
-  test.each<[string, string, string]>([
-    ["2023-01-01", "Asia/Tokyo", "2023-01-01 00:00:00"],
-  ])(
-    `formatDate(%s, %s, ${DATE_FORMAT.HUMAN_READABLE}): %s`,
-    (dateString, timezone, expected) => {
-      const dateFormat = DATE_FORMAT.HUMAN_READABLE;
-      expect(formatUTCDate(dateString, timezone, dateFormat)).toBe(expected);
-    }
-  );
-});
-
 describe("calcTimeDiff", () => {
   test.each<[Date, Date, number]>([
     [new Date("2021-01-01T00:00:00Z"), new Date("2021-01-01T01:00:00Z"), 60],
